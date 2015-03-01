417023209c3dd24fbca2720021cba707d4cc227c
SRUJAN KUMAR KEDALA <srujankk@gmail.com>
2015-03-01 18:55:48 -0500

    Updated comments for App.js on server side

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-/* jslint node:true */
-
-/* Requiring all the packages */
+/**
+ * jslint node:true
+ */
+/**
+ * Requiring all the required modules
+ */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -11,16 +14,24 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var stocks = require('./routes/stocks');
 
+/**
+ * Application object
+ */
 var app = express();
 
-/*  View engine setup */
+/**
+ * Setting up path or location for views / templates
+ * Setting up the view engine to be used - Jade
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 /* uncomment after placing your favicon in /public */
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
-/* Adding / Using the required Middleware */
+/**
+ * Using the required middleware
+ */
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -28,22 +39,35 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 
+/**
+ * Applying the header values for the response
+ * TODO: Can be removed if not required
+ */
 app.use('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
 
-/* Middleware for accessing static files html / CSS / images */
+/**
+ * Using the static middleware
+ * set the location or path for the static files
+ * static files: HTML, CSS, Images
+ * resources used by the client side application
+ */
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* Adding the routes */
+/**
+ * middleware for routes
+ */
 app.use('/', routes);
 app.use('/users', users);
 app.use('/stocks', stocks);
 
-/* Middleware for Error Handling
-   catch 404 and forward to error handler */
+/**
+ * Middleware for Error Handling
+ * Catch 404 error
+ */
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -74,4 +98,7 @@ app.use(function (err, req, res, next) {
     });
 });
 
+/**
+ * Export the application object in the module
+ */
 module.exports = app;
