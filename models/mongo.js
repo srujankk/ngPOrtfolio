faddd3fbfbaae9718585bdd3888ccd1228d48e70
SRUJAN KUMAR KEDALA <srujankk@gmail.com>
2015-03-08 20:54:06 -0400

    updated log on connection to database

--- models/mongo.js
+++ models/mongo.js
@@ -1,9 +1,20 @@
 var mongoose = require('mongoose'),
-    config = require('../config.js')
+    config = require('../config.js'),
+    mongoOptions = {
+        db: {
+            safe: true
+        }
+    };
 
 /**
  * Db connection for mongoose
  */
-mongoose.connect(config.dbConn);
+mongoose.connect(config.dbConn, mongoOptions, function (err, res) {
+    if (err) {
+        console.log('Error connecting to: ' + config.dbConn + ' > ' + err)
+    } else {
+        console.log('Success Connecting to: ' + config.dbConn);
+    }
+});
 
 module.exports = mongoose;
